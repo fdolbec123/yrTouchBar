ef7e9e0ee58b549c4979d3e6d61e2f42bb5fb6be
Francis Dolbec <fdolbec123@gmail.com>
2019-09-29 03:04:00 -0400

    Initial Commit

---
+++ yrTouchBar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  yrTouchBar
+//
+//  Created by Francis Dolbec on 2019-09-29.
+//  Copyright © 2019 Francis Dolbec. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
